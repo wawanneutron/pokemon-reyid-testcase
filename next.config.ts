995ebd1f1a39b261a111532b19319bce0cde82df
wawanneutron <wawanneutron1331@gmail.com>
2025-07-29 23:07:54 +0700

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,43 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname:
+          '/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname:
+          '/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/master/sprites/pokemon/other/home/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/**'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
