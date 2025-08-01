251f67feb5f9e17ad453e261f8300f293eb6d497
wawanneutron <wawanneutron1331@gmail.com>
2025-08-02 04:23:51 +0700

    adjust card compare

--- app/components/compare/CompareCard.tsx
+++ app/components/compare/CompareCard.tsx
@@ -14,17 +14,21 @@ import {
 import CloseIcon from '@mui/icons-material/Close'
 import { typeColors } from '@/app/types/color'
 import { PokemonCompareItem } from '@/app/types/compare'
-import Link from 'next/link'
-import { formatHeight, formatWeight } from '@/app/lib/utils'
+import { convertSlugToText, formatHeight, formatWeight } from '@/app/lib/utils'
 
 interface CompareCardProps {
   pokemon: PokemonCompareItem
   onRemove: () => void
 }
 
+const onDetailPokemon = (pokemonId: number) => {
+  if (pokemonId) window.location.href = `/pokemon/${pokemonId}`
+}
+
 function CompareCard({ pokemon, onRemove }: CompareCardProps) {
   return (
     <Card
+      onClick={() => onDetailPokemon(pokemon.id)}
       sx={{
         position: 'relative',
         backgroundColor: 'transparent',
@@ -34,82 +38,100 @@ function CompareCard({ pokemon, onRemove }: CompareCardProps) {
         zIndex: 2
       }}
     >
-      <Link href={`/pokemon/${pokemon.name}`} passHref legacyBehavior>
-        <CardActionArea
+      <CardActionArea
+        sx={{
+          px: 1,
+          width: '300px',
+          height: '500px'
+        }}
+      >
+        <IconButton
+          onClick={(e) => {
+            e.stopPropagation()
+            e.preventDefault()
+            onRemove()
+          }}
           sx={{
-            px: 1,
-            pt: 2,
-            pb: 1
+            position: 'absolute',
+            top: 12,
+            right: 12,
+            color: 'error.main',
+            zIndex: 2
+          }}
+          size="small"
+        >
+          <CloseIcon />
+        </IconButton>
+
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          borderRadius={2}
+          height={150}
+          sx={{
+            backgroundColor: pokemon.imageUrl ? 'transparent' : '#f2f2f26e',
+            backdropFilter: 'blur(6px)'
           }}
         >
-          <IconButton
-            onClick={(e) => {
-              e.stopPropagation()
-              e.preventDefault()
-              onRemove()
-            }}
-            sx={{
-              position: 'absolute',
-              top: 12,
-              right: 12,
-              color: 'error.main'
-            }}
-            size="small"
-          >
-            <CloseIcon />
-          </IconButton>
-
-          <Box display="flex" justifyContent="center">
+          {pokemon.imageUrl ? (
             <Image
               src={pokemon.imageUrl}
               alt={pokemon.name}
               width={200}
               height={200}
             />
-          </Box>
-
-          <CardContent>
-            <Typography variant="body2">#{pokemon.displayId}</Typography>
-            <Typography variant="h5" sx={{ textTransform: 'capitalize' }}>
-              {pokemon.name}
+          ) : (
+            <Typography variant="caption">
+              Pokemon Picture Placeholder
             </Typography>
-            <Typography variant="body2">
-              Weight: {formatWeight(pokemon.weight)}
-            </Typography>
-            <Typography variant="body2">
-              Height: {formatHeight(pokemon.height)}
-            </Typography>
+          )}
+        </Box>
 
-            <ul>
-              <Typography variant="body1" fontWeight="bold">
-                Abilities:{' '}
-              </Typography>
-              {pokemon.abilities.map((ability, index) => (
-                <li key={index}>
-                  <Typography variant="body2">
-                    {ability.name} {ability.isHidden ? '(Hidden)' : ''}
-                  </Typography>
-                </li>
-              ))}
-            </ul>
+        <CardContent>
+          <Typography variant="body2">#{pokemon.displayId}</Typography>
+          <Typography
+            variant="h5"
+            sx={{ textTransform: 'capitalize', marginBottom: '10px' }}
+          >
+            {convertSlugToText(pokemon.name)}
+          </Typography>
+          <Typography variant="body2">
+            Weight: {formatWeight(pokemon.weight)}
+          </Typography>
+          <Typography variant="body2">
+            Height: {formatHeight(pokemon.height)}
+          </Typography>
 
-            <Stack direction="row" gap={1} flexWrap="wrap">
-              {pokemon.types.map((type, tIdx) => (
-                <Chip
-                  key={tIdx}
-                  label={type}
-                  sx={{
-                    bgcolor: typeColors[type],
-                    color: '#fff',
-                    fontWeight: 'bold',
-                    px: 1
-                  }}
-                />
-              ))}
-            </Stack>
-          </CardContent>
-        </CardActionArea>
-      </Link>
+          <ul>
+            <Typography variant="body1" fontWeight="bold">
+              Abilities:{' '}
+            </Typography>
+            {pokemon.abilities.map((ability, index) => (
+              <li key={index}>
+                <Typography variant="body2">
+                  {ability.name} {ability.isHidden ? '(Hidden)' : ''}
+                </Typography>
+              </li>
+            ))}
+          </ul>
+
+          <Stack direction="row" gap={1} flexWrap="wrap">
+            {pokemon.types.map((type, tIdx) => (
+              <Chip
+                key={tIdx}
+                label={type}
+                sx={{
+                  bgcolor: typeColors[type],
+                  color: '#fff',
+                  fontWeight: 'bold',
+                  px: 1
+                }}
+              />
+            ))}
+          </Stack>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 }
